8f7d78b26056fcf49fcdf57abc36bb120bff8101
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-07-14 22:31:12 +0600

    Slight change in mail file (Quit issue)

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ while(1):
     print("2. Play with computer")
     print("3. Quit")
     play_with = int(input("Your answer (1/2/3)): "))
+    if play_with==3:
+        print("See you soon!")
+        break
+    
     winning_score = int(input("Please set a winning score. To leave it default (100) enter 0: "))
 
     player1 = 0
@@ -15,9 +19,6 @@ while(1):
     who = 1
     
     if play_with==1 or play_with==2:
-        play(player1,player2,lastone,number,who,play_with, winning_score)
-    elif play_with==3:
-        print("See you soon!")
-        break
+        play(player1,player2,lastone,number,who,play_with, winning_score)        
     else:
         print("Invalid response. Please choose again.")
